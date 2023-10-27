e8367545980d512b548ce2dcf72b787cdd74667c
priyankarepos <priyankasuryavanshi@promactinfo.com>
2023-10-27 18:00:40 +0530

    admin dashboard changes

--- src/Admin/AdminDashboard.js
+++ src/Admin/AdminDashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Outlet, NavLink } from 'react-router-dom';
+import { Outlet, Link, NavLink } from 'react-router-dom';
 import {
   Drawer,
   List,
