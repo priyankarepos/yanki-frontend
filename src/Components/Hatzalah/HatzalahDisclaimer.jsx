8d7830153749a00f4cd04801fa1bd5abe0304315
ChiragLabha <chiraglabha@promactinfo.com>
2024-03-15 13:00:22 +0530

    Added Hatzalah video and disclaimer

---
+++ src/Components/Hatzalah/HatzalahDisclaimer.jsx
@@ -0,0 +1,98 @@
+import { Box, Typography } from "@mui/material";
+
+const HatzalahDisclaimer = () => {
+  return (
+    <>
+      <Box>
+        <Typography component="div" style={{ marginBottom: "15px" }}>
+          Please be advised that Hatzalah of Los Angeles and YankiAI
+          (hereinafter referred to as "We," "Us," or "Our") strive to provide
+          the highest quality emergency medical services and information.
+          However, the effectiveness of any medical intervention cannot be
+          guaranteed. The information and resources provided by our
+          organization, including but not limited to instructional videos,
+          educational content, and guidance on emergency procedures, are
+          intended for general information purposes only and should not be
+          considered professional medical advice or a substitute for
+          professional medical care, diagnosis, or treatment.
+        </Typography>
+
+        <Typography component="div" style={{ marginBottom: "15px" }}>
+          This information is designed to support, not replace, the relationship
+          that exists between a patient/site visitor and his/her existing
+          healthcare provider. We cannot ensure the saving of a life in every
+          circumstance as individual cases may vary, and the outcomes of
+          emergency interventions depend on factors beyond our control.
+        </Typography>
+
+        <Typography component="div" style={{ marginBottom: "15px" }}>
+          The system we have developed to connect individuals with Hatzalah
+          centers and responders worldwide is intended to facilitate access to
+          emergency services but does not replace professional medical advice,
+          diagnosis, or treatment. Always seek the advice of your physician or
+          another qualified health provider with any questions you may have
+          regarding a medical condition. Do not disregard, avoid, or delay
+          obtaining medical advice from qualified healthcare providers based on
+          the information provided.
+        </Typography>
+
+        <Typography component="div" style={{ marginBottom: "15px" }}>
+          In no event shall we be liable for any direct, indirect, incidental,
+          special, consequential, or punitive damages arising out of the use of
+          or inability to use the information and services provided. By
+          accessing and using these resources, you acknowledge and agree to this
+          disclaimer and assume full responsibility for the use of the
+          information and services.
+        </Typography>
+
+        <Typography component="div" style={{ marginBottom: "15px" }}>
+          We do not endorse, recommend, or guarantee the accuracy, reliability,
+          or quality of any content, information, materials, services, products,
+          opinions, or statements available through our service or linked sites.
+          We explicitly disclaim any warranties related to the service's
+          operation, including uninterrupted or error-free service or the
+          correction of defects.
+        </Typography>
+
+        <Typography component="div" style={{ marginBottom: "15px" }}>
+          We are not responsible for the content, accuracy, or quality of
+          products or services provided by third parties or advertised on
+          third-party services. It is acknowledged that we are not liable for
+          any defamatory, offensive, or illegal conduct of users or third
+          parties, with all risks associated with such content resting on the
+          user.
+        </Typography>
+
+        <Typography component="div" style={{ marginBottom: "15px" }}>
+          All products and services offered by us are provided "as is" without
+          any warranties, including but not limited to warranties of
+          merchantability, fitness for a particular purpose, or non-infringement
+          of intellectual property, whether express or implied.
+        </Typography>
+
+        <Typography component="div" style={{ marginBottom: "15px" }}>
+          The applicability of these disclaimers and the limitation of liability
+          may vary based on jurisdiction. Certain jurisdictions do not allow the
+          disclaimer of certain warranties or the limitation or exclusion of
+          liability for certain types of damages. Accordingly, some of the above
+          disclaimers, exclusions, or limitations may not apply to you, but they
+          shall apply to the maximum extent permitted by applicable law.
+        </Typography>
+
+        <Typography component="div" style={{ marginBottom: "15px" }}>
+          This information is provided for legal purposes only and does not
+          constitute medical advice. Always consult a qualified medical
+          professional for any health-related concerns or emergencies.
+        </Typography>
+
+        <Typography component="div" style={{ marginBottom: "15px" }}>
+          Your use of our services indicates your agreement to this disclaimer
+          and your understanding that you are using our services and information
+          at your own risk.
+        </Typography>
+      </Box>
+    </>
+  );
+};
+
+export default HatzalahDisclaimer;
---
+++ src/Components/Hatzalah/HatzalahVideo.jsx
@@ -0,0 +1,152 @@
+import React, { useState, useRef } from "react";
+import { Paper, Typography, Tooltip } from "@mui/material";
+import Box from "@mui/material/Box";
+import Carousel from "react-multi-carousel";
+import Vimeo from "@u-wave/react-vimeo";
+import { styled } from "@mui/system";
+import "react-multi-carousel/lib/styles.css";
+
+const StyledCarouselItem = styled("div")(({ theme }) => ({
+  padding: theme.spacing(2),
+  textAlign: "center",
+  background: theme.palette.background.paper,
+  border: `1px solid ${theme.palette.divider}`,
+  marginTop: "10px",
+  borderRadius: theme.shape.borderRadius,
+  "& iframe": {
+    width: "100%",
+    height: "200px",
+    border: 0,
+  },
+}));
+
+const HatzalahVideo = () => {
+  const [showCRPVideos, setShowCRPVideos] = useState(false);
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(null);
+  const playerRefs = useRef([]);
+  const handleCRPContentClick = (e) => {
+    e.stopPropagation();
+    setShowCRPVideos(true);
+  };
+
+  const vimeoVideoDetails = [
+    {
+      title: "road",
+      link: "https://player.vimeo.com/video/920561618?h=be3ac1cc2a",
+      embedLink:
+        "<iframe src=https://player.vimeo.com/video/920561618?badge=0&autopause=0&player_id=0&app_id=288468 width=576 height=1024 frameborder=0 allow=autoplay; fullscreen; picture-in-picture; clipboard-write title=road></iframe>",
+    },
+  ];
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 6000, min: 3000 },
+      items: 3,
+      slidesToSlide: 3,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  const handleVideoPlay = (index) => {
+    if (currentVideoIndex !== null && playerRefs.current[currentVideoIndex]) {
+      const currentVideoPlayer = playerRefs.current[currentVideoIndex];
+      if (currentVideoPlayer.pauseVideo) {
+        currentVideoPlayer.pauseVideo();
+      } else if (currentVideoPlayer.pause) {
+        currentVideoPlayer.pause();
+      }
+    }
+    setCurrentVideoIndex(index);
+  };
+
+  const onReady = (event, index) => {
+    playerRefs.current[index] = event.target;
+  };
+  return (
+    <>
+      <Box onClick={(e) => e.stopPropagation()}>
+        <Paper sx={{ p: 2 }}>
+          <Box>
+            <Typography
+              onClick={handleCRPContentClick}
+              className={`switch-button ${showCRPVideos ? "selected" : ""}`}
+            >
+              CRP
+            </Typography>
+            <Typography className="switch-button">AED</Typography>
+            <Typography className="switch-button">Choking</Typography>
+            <Typography className="switch-button">Allergies</Typography>
+            <Typography className="switch-button">Bleeding</Typography>
+          </Box>
+
+          <Carousel responsive={responsive}>
+            {showCRPVideos &&
+              vimeoVideoDetails.map((item, index) => (
+                <Paper key={item.title}>
+                  <StyledCarouselItem
+                    key={item.title}
+                    className="youtube-box"
+                    sx={{ marginRight: "5px" }}
+                  >
+                    {item.link && (
+                      <div>
+                        <Vimeo
+                          id={item.link.split("v=")[1]}
+                          onReady={(event) => onReady(event, index)}
+                          onPlay={() => handleVideoPlay(index)}
+                          video={item.link}
+                          width="100%"
+                          height="150px"
+                          autoplay={false}
+                          controls={true}
+                          showByline={false}
+                          showTitle={false}
+                          showPortrait={false}
+                          loop={false}
+                          autopause={true}
+                        />
+                      </div>
+                    )}
+                    <div>
+                      <Typography variant="h6" component="div">
+                        <Tooltip title={item.title}>
+                          <div
+                            style={{
+                              maxWidth: "100%",
+                              overflow: "hidden",
+                              textOverflow: "ellipsis",
+                              display: "-webkit-box",
+                              "-webkit-line-clamp": 2,
+                              "-webkit-box-orient": "vertical",
+                              textAlign: "left",
+                            }}
+                          >
+                            {item.title}
+                          </div>
+                        </Tooltip>
+                      </Typography>
+                    </div>
+                  </StyledCarouselItem>
+                </Paper>
+              ))}
+          </Carousel>
+        </Paper>
+      </Box>
+    </>
+  );
+};
+
+export default HatzalahVideo;
