23aa4f8031d0a3793bf7d60da58ffaad46e1ce47
ChiragLabhaPromact <151526430+ChiragLabhaPromact@users.noreply.github.com>
2024-04-25 18:09:26 +0530

    Revert "Add Markdown conversion functionality and fix home UI"

--- src/Components/SentenceAnswer.jsx
+++ src/Components/SentenceAnswer.jsx
@@ -5,19 +5,18 @@ import Paper from "@mui/material/Paper";
 import { useEffect, useState } from "react";
 import { Button, CircularProgress } from "@mui/material";
 import TextField from "@mui/material/TextField";
-import axios from "axios";
+import axios from 'axios';
 import "./AnswerStyle.scss";
-import ReactMarkdown from "react-markdown";
 
 const SentenceAnswer = ({ answer }) => {
   // const [showCandle, setShowCandle] = useState(true);
   const [processedContentResponse, setProcessedContentResponse] = useState([]);
   const [additionalMessage, setAdditionalMessage] = useState("");
   const [apiResponseMessage, setApiResponseMessage] = useState("");
-  const [apiContentResponseMessage, setContentApiResponseMessage] =
-    useState("");
+  const [apiContentResponseMessage, setContentApiResponseMessage] = useState("");
   const [loading, setLoading] = useState(false);
   const [loadingButtonIndex, setLoadingButtonIndex] = useState(null);
+
 
   // const toggleCandle = () => {
   //   setShowCandle((prev) => !prev);
@@ -29,7 +28,7 @@ const SentenceAnswer = ({ answer }) => {
         const answerArray = answer.contentResponse.split("\n");
         setProcessedContentResponse(answerArray);
       }
-    } catch (e) {}
+    } catch (e) { }
   }, [answer]);
 
   const handleTextFieldChange = (event) => {
@@ -52,8 +51,9 @@ const SentenceAnswer = ({ answer }) => {
         }
       );
 
+      console.log("API Response:", response.data);
       setApiResponseMessage(response.data.message);
-      setContentApiResponseMessage(response.data.contentResponse);
+      setContentApiResponseMessage(response.data.contentResponse)
 
       setAdditionalMessage("");
     } catch (error) {
@@ -64,15 +64,11 @@ const SentenceAnswer = ({ answer }) => {
     }
   };
 
+
   const renderContentResponse = () => {
     return processedContentResponse.map((ans, index) => (
-      <Typography
-        variant="h6"
-        component="div"
-        key={index}
-        className="sentence-answer-container"
-      >
-        <ReactMarkdown>{renderClickableContent(ans)}</ReactMarkdown>
+      <Typography variant="h6" component="div" key={index} className="sentence-answer-container">
+        {renderClickableContent(ans)}
       </Typography>
     ));
   };
@@ -81,20 +77,18 @@ const SentenceAnswer = ({ answer }) => {
     const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
     const phoneRegex = /\b\d{10,}\b/g;
     const urlRegex = /\b(?:https?|ftp):\/\/\S+|\bwww\.\S+\.com\b/g;
-
+  
     let content = [];
-
-    const cleanedText = text.replace(/(^-|^\.)+/gm, (match) =>
-      match.replace(/[-.]/g, "")
-    );
-
+  
+    const cleanedText = text.replace(/(^-|^\.)+/gm, match => match.replace(/[-.]/g, ''));
+  
     cleanedText.split(/\s+/).forEach((word, index, array) => {
       if (word.match(emailRegex)) {
         content.push(
           <span
             key={index}
-            className="enterprise-pdf-link"
-            onClick={() => (window.location.href = `mailto:${word}`)}
+            className='enterprise-pdf-link'
+            onClick={() => window.location.href = `mailto:${word}`}
           >
             {word}
           </span>
@@ -106,8 +100,8 @@ const SentenceAnswer = ({ answer }) => {
         content.push(
           <span
             key={index}
-            className="enterprise-pdf-link"
-            onClick={() => (window.location.href = `tel:${word}`)}
+            className='enterprise-pdf-link'
+            onClick={() => window.location.href = `tel:${word}`}
           >
             {word}
           </span>
@@ -120,8 +114,8 @@ const SentenceAnswer = ({ answer }) => {
         content.push(
           <span
             key={index}
-            className="enterprise-pdf-link"
-            onClick={() => window.open(word, "_blank")}
+            className='enterprise-pdf-link'
+            onClick={() => window.open(word, '_blank')}
           >
             {word}
           </span>
@@ -131,7 +125,7 @@ const SentenceAnswer = ({ answer }) => {
           content.push(" ");
         }
       } else {
-        const punctuation = [".", ",", "-"];
+        const punctuation = ['.', ',', '-'];
         let cleanedWord = word;
         let lastChar = "";
         // Check if the word ends with punctuation, and remove only comma and dot
@@ -141,15 +135,19 @@ const SentenceAnswer = ({ answer }) => {
         }
         content.push(
           <span key={index}>
-            {cleanedWord}
-            {lastChar}{" "}
+            {cleanedWord}{lastChar}{" "}
           </span>
         );
       }
     });
-
+    
     return content;
   };
+  
+  
+  
+  
+  
 
   return (
     <>
@@ -167,11 +165,7 @@ const SentenceAnswer = ({ answer }) => {
             />
           )} */}
 
-          <Typography
-            variant="h6"
-            component="div"
-            className="sentence-answer-container"
-          >
+          <Typography variant="h6" component="div" className="sentence-answer-container">
             {answer?.message}
           </Typography>
           {renderContentResponse()}
@@ -184,11 +178,7 @@ const SentenceAnswer = ({ answer }) => {
                 margin="normal"
                 value={additionalMessage}
                 onChange={handleTextFieldChange}
-                disabled={
-                  loading ||
-                  !answer?.isMail === true ||
-                  apiResponseMessage !== ""
-                }
+                disabled={loading || !answer?.isMail === true  || apiResponseMessage !==""}
               />
               {answer.userType.map((type, index) => (
                 <Button
@@ -197,12 +187,7 @@ const SentenceAnswer = ({ answer }) => {
                   color="primary"
                   onClick={() => handleUserTypeButtonClick(type, index)}
                   className="sentence-answer-button"
-                  disabled={
-                    loading ||
-                    loadingButtonIndex === index ||
-                    !answer?.isMail === true ||
-                    apiResponseMessage !== ""
-                  }
+                  disabled={loading || loadingButtonIndex === index || !answer?.isMail === true || apiResponseMessage !==""}
                 >
                   {loading && loadingButtonIndex === index ? (
                     <CircularProgress size={24} className="color-white" />
@@ -227,24 +212,20 @@ const SentenceAnswer = ({ answer }) => {
             </Box>
           )}
         </Paper>
-        {apiContentResponseMessage && (
-          <Paper
-            sx={{
-              p: 2,
-            }}
-          >
-            <Typography
-              variant="body1"
-              component="div"
-              className="sentence-answer-response"
-            >
-              {apiContentResponseMessage}
-            </Typography>
-          </Paper>
-        )}
+        {apiContentResponseMessage && <Paper sx={{
+          p: 2,
+        }}>
+          <Typography
+            variant="body1"
+            component="div"
+            className="sentence-answer-response"
+          >{apiContentResponseMessage}</Typography>
+        </Paper>}
       </Box>
     </>
   );
 };
 
 export default SentenceAnswer;
+
+
