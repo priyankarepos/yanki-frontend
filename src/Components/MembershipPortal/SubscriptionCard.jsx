fbd59ff3a827358b533da230c3357cb62dee4969
priyankarepos <priyankasuryavanshi@promactinfo.com>
2024-05-22 13:09:16 +0530

    Add loader in subscribe and upgrade button, remove admin redirection to membership portal after login

--- src/Components/MembershipPortal/SubscriptionCard.jsx
+++ src/Components/MembershipPortal/SubscriptionCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, CircularProgress, Grid, Typography } from "@mui/material";
 import './SubscriptionCard.scss';
 import { Context } from '../../App';
 
-const SubscriptionCard = ({ tier, message, onClick, task, taskCost, isSubscribed, price, isPlanSubscribed }) => {
+const SubscriptionCard = ({ tier, message, onClick, task, taskCost, isSubscribed, price, isPlanSubscribed, upgradeLoading }) => {
   const { activeTab } = React.useContext(Context);
+  const getClassNames = () => {
+    let className = activeTab === 0 ? 'subscription-card' : 'subscription-card-light';
+    if (isSubscribed) {
+      className += ' Subscribed-Active-Card';
+    }
+    return className;
+  };
+
+  const cardClassName = getClassNames();
   return (
     <Grid item xs={12} sm={6} md={4} lg={4}>
-      <Box className={activeTab === 0 ? 'subscription-card' : 'subscription-card-light'}>
+      <Box className={cardClassName}>
         <Typography variant="h6" className="tier-name">{tier}</Typography>
         <Typography variant="h6" className="tier-name tier-price">${price}</Typography>
         <ul className="description-list">
@@ -15,8 +24,8 @@ const SubscriptionCard = ({ tier, message, onClick, task, taskCost, isSubscribed
           <li className="description-item">{task}</li>
           <li className="description-item">{taskCost}</li>
         </ul>
-        <Button onClick={onClick} variant="contained" className="subscribe-btn" disabled={isSubscribed}>
-          {(isPlanSubscribed ? (isSubscribed ? "Current Plan" : "Upgrade Plan") : "Subscribe")}
+        <Button onClick={onClick} variant="contained" className="subscribe-btn" disabled={isSubscribed || upgradeLoading}>
+          {upgradeLoading ? <CircularProgress size={24} /> : (isPlanSubscribed ? (isSubscribed ? "Current Plan" : "Upgrade Plan") : "Subscribe")}
         </Button>
       </Box>
     </Grid>
