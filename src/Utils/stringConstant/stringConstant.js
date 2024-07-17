f48e41fe4dea0682b660377afaea239fa93b5589
priyankarepos <priyankasuryavanshi@promactinfo.com>
2024-07-17 19:01:16 +0530

    Update code according to api changes

--- src/Utils/stringConstant/stringConstant.js
+++ src/Utils/stringConstant/stringConstant.js
@@ -1,22 +1,25 @@
 
 export const messages = {
-    mikvahDetailsError : "Error fetching mikvah details:",
-    govadenAnswerTooltip : "govaden-answer-tooltip",
-    mikvahDetailBox : "mikvah-detail-box",
-    mikvahDetailInfo : "mikvah-detail-info",
-    mikvahDetailInfoLogo : "mikvah-detail-info-logo",
-    mikvahDetailInfoContent : "mikvah-detail-info-content",
-    collapsibleTable : "collapsible table",
-    notAvailable : "NA",
-    cursorPointer : "pointer",
-    enterprisePaginationClass : "enterprise-pagination",
-    primaryColor : "primary",
-    eventFormSubmitButtonLoader : "event-form-submit-button-loader",
+    mikvahDetailsError: "Error fetching mikvah details:",
+    govadenAnswerTooltip: "govaden-answer-tooltip",
+    mikvahDetailBox: "mikvah-detail-box",
+    mikvahDetailInfo: "mikvah-detail-info",
+    mikvahDetailInfoLogo: "mikvah-detail-info-logo",
+    mikvahDetailInfoContent: "mikvah-detail-info-content",
+    collapsibleTable: "collapsible table",
+    notAvailable: "NA",
+    cursorPointer: "pointer",
+    enterprisePaginationClass: "enterprise-pagination",
+    primaryColor: "primary",
+    eventFormSubmitButtonLoader: "event-form-submit-button-loader",
+    fetchError: 'Error fetching data:',
 };
 
 export const apiUrls = {
- mikvahDetails: `${process.env.REACT_APP_API_HOST}/api/mikvah/mikvah-details`,
- indexAndUpload: (pdfName, keywords) => `${process.env.REACT_APP_API_HOST}/api/JewishPrayerTextIndex/index-and-upload?PdfName=${encodeURIComponent(pdfName)}&Keywords=${encodeURIComponent(JSON.stringify(keywords))}`,
- updateDocumentKeywords: (pdfId, tags) => 
-    `${process.env.REACT_APP_API_HOST}/api/JewishPrayerTextIndex/update-document-keywords?documentId=${encodeURIComponent(pdfId)}&newKeywords=${encodeURIComponent(JSON.stringify(tags))}`
+    mikvahDetails: `${process.env.REACT_APP_API_HOST}/api/mikvah/mikvah-details`,
+    indexAndUpload: (pdfName, keywords) => `${process.env.REACT_APP_API_HOST}/api/JewishPrayerTextIndex/index-and-upload?PdfName=${encodeURIComponent(pdfName)}&Keywords=${encodeURIComponent(JSON.stringify(keywords))}`,
+    updateDocumentKeywords: (pdfId, tags) =>
+        `${process.env.REACT_APP_API_HOST}/api/JewishPrayerTextIndex/update-document-keywords?documentId=${encodeURIComponent(pdfId)}&newKeywords=${encodeURIComponent(JSON.stringify(tags))}`,
+    documentMapping: (pageNumber) =>
+        `${process.env.REACT_APP_API_HOST}/api/JewishPrayerTextIndex/document-mapping?pageNumber=${pageNumber}&pageSize=10`,
 };
